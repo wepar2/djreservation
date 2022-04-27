a535d48293be4e2d87a97456e018749fdb0b3d4d
wepar2 <83143626+wepar2@users.noreply.github.com>
2022-04-27 14:00:22 +0200

    Update admin.py

--- demo/demoapp/admin.py
+++ demo/demoapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import MyObject
 
 # Register your models here.
+
+class myobjectAdmin(admin.ModelAdmin):
+
+	list_display = ['name','quantity', 'measurement_unit']
+
+
+admin.site.register(MyObject, myobjectAdmin)
